036b10c7b673d6c874b5fbd9b835f725110df005
Sway Ducky <swayducky@gmail.com>
2023-10-08 08:36:58 -0700

    simplify

--- README.md
+++ README.md
@@ -14,7 +14,6 @@ Forked from "Obsidian Clipper": https://github.com/jplattel/obsidian-clipper
     - Try opening: `https://jplattel.github.io/obsidian-clipper/clip.html`
     - Try specifying the vault name: `https://jplattel.github.io/obsidian-clipper/clip.html?vault=<vaultname>`
 
-
 ## Technical explanation
 
 This clipper is made possible with a work-around, since Chrome Extensions are forbidden to open custom url-schemes directly. The way around this issue is a custom html page that is hosted on Github-pages and also included in the repository: `docs/clip.html`. This little file contains javascript that pulls the data like vault & note out of the url params. With this data, it reconstructs the obsidian url and opens the right note!
---
+++ src/core/background.js
@@ -0,0 +1,30 @@
+
+chrome.action.onClicked.addListener(async function (tab) {
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        files: [
+            "lib/webbrowser-polyfill.js",
+            "lib/jquery.js",
+            "lib/rangy.js",
+            "lib/moment.js",
+            "lib/turndown.js",
+        ]
+    }, () => {
+        chrome.scripting.executeScript({
+            target: {tabId: tab.id},
+            files: ['run.js']
+        })
+    })
+});
+
+chrome.runtime.onMessage.addListener(async function listener(result) {
+    console.log("[Clipper.onMessage]", result)
+    alert("Clipper.onMessage" + JSON.stringify(result))
+});
+
+// On install, open the options page
+chrome.runtime.onInstalled.addListener(function (object) {
+    if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+        chrome.tabs.create({ url: chrome.runtime.getURL("options.html") }, function (tab) {});
+    }
+});
